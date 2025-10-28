1f45129782a97e3b1df2fb7e396beb6e9e141c19
Graeme077 <graybujuu@gmail.com>
2025-10-28 12:03:44 +0100

    deleted some files

--- page3.html/script.js
+++
@@ -1,9 +0,0 @@
-
-function confirmName() {
-  let name = document.getElementById("nameField").value.trim();
-  if (name === "") {
-    alert("Please enter a valid name.");
-  } else {
-    alert("Welcome " + name + "!");
-  }
-}
